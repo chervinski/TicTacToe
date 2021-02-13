db723411bd666c37f762275078177cf8a802590a
Max <unfusions@gmail.com>
2021-02-13 21:27:08 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace TicTacToe
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.button9 = new System.Windows.Forms.Button();
+			this.button8 = new System.Windows.Forms.Button();
+			this.button7 = new System.Windows.Forms.Button();
+			this.button6 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button1 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.connectButton = new System.Windows.Forms.Button();
+			this.openButton = new System.Windows.Forms.Button();
+			this.closeButton = new System.Windows.Forms.Button();
+			this.localIPLabel = new System.Windows.Forms.Label();
+			this.disconnectButton = new System.Windows.Forms.Button();
+			this.turnLabel = new System.Windows.Forms.Label();
+			this.panel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this.button9);
+			this.panel1.Controls.Add(this.button8);
+			this.panel1.Controls.Add(this.button7);
+			this.panel1.Controls.Add(this.button6);
+			this.panel1.Controls.Add(this.button5);
+			this.panel1.Controls.Add(this.button4);
+			this.panel1.Controls.Add(this.button3);
+			this.panel1.Controls.Add(this.button2);
+			this.panel1.Controls.Add(this.button1);
+			this.panel1.Location = new System.Drawing.Point(12, 120);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(318, 318);
+			this.panel1.TabIndex = 0;
+			// 
+			// button9
+			// 
+			this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button9.Location = new System.Drawing.Point(215, 215);
+			this.button9.Name = "button9";
+			this.button9.Size = new System.Drawing.Size(100, 100);
+			this.button9.TabIndex = 9;
+			this.button9.UseVisualStyleBackColor = true;
+			this.button9.Click += new System.EventHandler(this.Cell_Click);
+			// 
+			// button8
+			// 
+			this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button8.Location = new System.Drawing.Point(109, 215);
+			this.button8.Name = "button8";
+			this.button8.Size = new System.Drawing.Size(100, 100);
+			this.button8.TabIndex = 8;
+			this.button8.UseVisualStyleBackColor = true;
+			this.button8.Click += new System.EventHandler(this.Cell_Click);
+			// 
+			// button7
+			// 
+			this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button7.Location = new System.Drawing.Point(3, 215);
+			this.button7.Name = "button7";
+			this.button7.Size = new System.Drawing.Size(100, 100);
+			this.button7.TabIndex = 7;
+			this.button7.UseVisualStyleBackColor = true;
+			this.button7.Click += new System.EventHandler(this.Cell_Click);
+			// 
+			// button6
+			// 
+			this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button6.Location = new System.Drawing.Point(215, 109);
+			this.button6.Name = "button6";
+			this.button6.Size = new System.Drawing.Size(100, 100);
+			this.button6.TabIndex = 6;
+			this.button6.UseVisualStyleBackColor = true;
+			this.button6.Click += new System.EventHandler(this.Cell_Click);
+			// 
+			// button5
+			// 
+			this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button5.Location = new System.Drawing.Point(109, 109);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(100, 100);
+			this.button5.TabIndex = 5;
+			this.button5.UseVisualStyleBackColor = true;
+			this.button5.Click += new System.EventHandler(this.Cell_Click);
+			// 
+			// button4
+			// 
+			this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button4.Location = new System.Drawing.Point(3, 109);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(100, 100);
+			this.button4.TabIndex = 4;
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.Cell_Click);
+			// 
+			// button3
+			// 
+			this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button3.Location = new System.Drawing.Point(215, 3);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(100, 100);
+			this.button3.TabIndex = 3;
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.Cell_Click);
+			// 
+			// button2
+			// 
+			this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button2.Location = new System.Drawing.Point(109, 3);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(100, 100);
+			this.button2.TabIndex = 2;
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Cell_Click);
+			// 
+			// button1
+			// 
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button1.Location = new System.Drawing.Point(3, 3);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(100, 100);
+			this.button1.TabIndex = 1;
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Cell_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(20, 13);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "IP:";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(38, 6);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 20);
+			this.textBox1.TabIndex = 2;
+			// 
+			// connectButton
+			// 
+			this.connectButton.Location = new System.Drawing.Point(146, 4);
+			this.connectButton.Name = "connectButton";
+			this.connectButton.Size = new System.Drawing.Size(75, 23);
+			this.connectButton.TabIndex = 3;
+			this.connectButton.Text = "Connect";
+			this.connectButton.UseVisualStyleBackColor = true;
+			this.connectButton.Click += new System.EventHandler(this.Connect_Click);
+			// 
+			// openButton
+			// 
+			this.openButton.Location = new System.Drawing.Point(15, 32);
+			this.openButton.Name = "openButton";
+			this.openButton.Size = new System.Drawing.Size(75, 23);
+			this.openButton.TabIndex = 5;
+			this.openButton.Text = "Open server";
+			this.openButton.UseVisualStyleBackColor = true;
+			this.openButton.Click += new System.EventHandler(this.Open_Click);
+			// 
+			// closeButton
+			// 
+			this.closeButton.Enabled = false;
+			this.closeButton.Location = new System.Drawing.Point(15, 61);
+			this.closeButton.Name = "closeButton";
+			this.closeButton.Size = new System.Drawing.Size(75, 23);
+			this.closeButton.TabIndex = 6;
+			this.closeButton.Text = "Close server";
+			this.closeButton.UseVisualStyleBackColor = true;
+			this.closeButton.Click += new System.EventHandler(this.Disconnect_Click);
+			// 
+			// label2
+			// 
+			this.localIPLabel.AutoSize = true;
+			this.localIPLabel.Location = new System.Drawing.Point(96, 51);
+			this.localIPLabel.Name = "label2";
+			this.localIPLabel.Size = new System.Drawing.Size(0, 13);
+			this.localIPLabel.TabIndex = 7;
+			// 
+			// disconnectButton
+			// 
+			this.disconnectButton.Enabled = false;
+			this.disconnectButton.Location = new System.Drawing.Point(227, 4);
+			this.disconnectButton.Name = "disconnectButton";
+			this.disconnectButton.Size = new System.Drawing.Size(75, 23);
+			this.disconnectButton.TabIndex = 4;
+			this.disconnectButton.Text = "Disconnect";
+			this.disconnectButton.UseVisualStyleBackColor = true;
+			this.disconnectButton.Click += new System.EventHandler(this.Disconnect_Click);
+			// 
+			// label3
+			// 
+			this.turnLabel.AutoSize = true;
+			this.turnLabel.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.turnLabel.Location = new System.Drawing.Point(11, 98);
+			this.turnLabel.Name = "label3";
+			this.turnLabel.Size = new System.Drawing.Size(0, 22);
+			this.turnLabel.TabIndex = 9;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(342, 450);
+			this.Controls.Add(this.turnLabel);
+			this.Controls.Add(this.disconnectButton);
+			this.Controls.Add(this.localIPLabel);
+			this.Controls.Add(this.closeButton);
+			this.Controls.Add(this.openButton);
+			this.Controls.Add(this.connectButton);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.panel1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MaximizeBox = false;
+			this.Name = "Form1";
+			this.Text = "Tic-Tac-Toe";
+			this.panel1.ResumeLayout(false);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button9;
+		private System.Windows.Forms.Button button8;
+		private System.Windows.Forms.Button button7;
+		private System.Windows.Forms.Button button6;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button connectButton;
+		private System.Windows.Forms.Button openButton;
+		private System.Windows.Forms.Button closeButton;
+		private System.Windows.Forms.Label localIPLabel;
+		private System.Windows.Forms.Button disconnectButton;
+		private System.Windows.Forms.Label turnLabel;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+	public partial class Form1 : Form
+	{
+		private static readonly char cross = '❌', nought = '◯';
+		private static readonly int port = 8888;
+
+		private char symbol;
+		private bool turn, disconnect;
+		private Socket server, handler;
+		private bool Turn
+		{
+			get => turn;
+			set
+			{
+				turn = value;
+				turnLabel.Text = turn ? $"Your turn ({symbol})" : $"Waiting for turn ({(symbol == cross ? nought : cross)})";
+			}
+		}
+		public Form1() => InitializeComponent();
+		private void Reset()
+		{
+			symbol = '\0';
+			foreach (Button button in panel1.Controls)
+				button.Text = string.Empty;
+			openButton.Enabled = connectButton.Enabled = true;
+			closeButton.Enabled = disconnectButton.Enabled = false;
+			localIPLabel.Text = turnLabel.Text = string.Empty;
+		}
+		private bool HandleWinner()
+		{
+			char[] cells = panel1.Controls.Cast<Button>().Select(x => x.Text == string.Empty ? '\0' : x.Text[0]).ToArray();
+			char winner;
+
+			for (int i = 0; i < 9; i += 3)
+				if (cells[i] != '\0' && cells[i] == cells[i + 1] && cells[i] == cells[i + 2])
+				{
+					winner = cells[i];
+					goto end;
+				}
+
+			for (int i = 0; i < 3; ++i)
+				if (cells[i] != '\0' && cells[i] == cells[i + 3] && cells[i] == cells[i + 6])
+				{
+					winner = cells[i];
+					goto end;
+				}
+
+			winner = cells[4] != '\0' && (
+				cells[4] == cells[0] && cells[4] == cells[8] ||
+				cells[4] == cells[2] && cells[4] == cells[6]) ? cells[4] : '\0';
+
+			end:
+			bool draw = winner == '\0' && cells.All(x => x != '\0');
+			if (winner != '\0' || draw)
+			{
+				MessageBox.Show(draw ? "Draw!" : winner + " won!");
+				foreach (Button b in panel1.Controls)
+					b.Text = string.Empty;
+				Turn = symbol == cross;
+				return true;
+			}
+			return false;
+		}
+		private void Cell_Click(object sender, EventArgs e)
+		{
+			Button button = sender as Button;
+			if (button.Text == string.Empty && symbol != '\0' && Turn)
+			{
+				handler.Send(new []{ (byte)panel1.Controls.IndexOf(button) });
+				button.Text = symbol.ToString();
+
+				if (!HandleWinner())
+					Turn = false;
+			}
+		}
+		private async void Connect_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				using (handler = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+				{
+					handler.Connect(new IPEndPoint(IPAddress.Parse(textBox1.Text), port));
+
+					symbol = nought;
+					Turn = false;
+					disconnectButton.Enabled = true;
+					connectButton.Enabled = openButton.Enabled = false;
+
+					await ListenAsync();
+				}
+			}
+			catch (Exception ex)
+			{
+				if (disconnect) disconnect = false;
+				else MessageBox.Show(ex.Message);
+				Reset();
+			}
+		}
+		private async void Open_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				localIPLabel.Text = "Your local IP: " + Dns.GetHostEntry(Dns.GetHostName()).AddressList.First(x => x.AddressFamily == AddressFamily.InterNetwork).ToString();
+
+				closeButton.Enabled = true;
+				connectButton.Enabled = openButton.Enabled = false;
+
+				using (server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+				{
+					server.Bind(new IPEndPoint(IPAddress.Any, port));
+					server.Listen(1);
+					using (handler = await server.AcceptAsync())
+					{
+						symbol = cross;
+						Turn = true;
+						await ListenAsync();
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				if (disconnect) disconnect = false;
+				else MessageBox.Show(ex.Message);
+				Reset();
+			}
+		}
+		private async Task ListenAsync()
+		{
+			while (true)
+			{
+				ArraySegment<byte> segment = new ArraySegment<byte>(new byte[1]);
+				await handler.ReceiveAsync(segment, SocketFlags.None);
+				byte message = segment.Array[0];
+
+				panel1.Controls[message].Text = (symbol == cross ? nought : cross).ToString();
+				if (!HandleWinner())
+					Turn = true;
+			}
+		}
+		private void Disconnect_Click(object sender, EventArgs e)
+		{
+			disconnect = true;
+			server?.Close();
+			handler?.Close();
+		}
+	}
+}
